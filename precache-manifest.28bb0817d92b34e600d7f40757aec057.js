53a258817fb18885dc8adec362ee15cf895d7907
Raulks <rksole90@gmail.com>
2020-09-23 20:13:20 +0200

    Updates

---
+++ precache-manifest.28bb0817d92b34e600d7f40757aec057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71aed83d10e5a1491c777ed1227c1a4f",
+    "url": "/InsuranceQuotes/index.html"
+  },
+  {
+    "revision": "2867429896a8da59a16b",
+    "url": "/InsuranceQuotes/static/css/main.2a816896.chunk.css"
+  },
+  {
+    "revision": "8ab5d9a7c935b8ef7c27",
+    "url": "/InsuranceQuotes/static/js/2.7cd5b041.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/InsuranceQuotes/static/js/2.7cd5b041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2867429896a8da59a16b",
+    "url": "/InsuranceQuotes/static/js/main.a94de048.chunk.js"
+  },
+  {
+    "revision": "89f84b80a874b9b34c44",
+    "url": "/InsuranceQuotes/static/js/runtime-main.23426d80.js"
+  }
+]);
